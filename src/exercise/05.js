ca307b33719d2558d7cdab8c2d0aef5c7c403bb8
Joel Mathew Koshy <joelmathewkoshy@gmail.com>
2022-12-16 14:13:50 +0530

    Styling

--- src/exercise/05.js
+++ src/exercise/05.js
@@ -14,9 +14,36 @@ import '../box-styles.css'
 // 🐨 also use the style prop to make the font italic
 // 💰 Here are available style attributes: backgroundColor, fontStyle
 
-const smallBox = <div>small lightblue box</div>
-const mediumBox = <div>medium pink box</div>
-const largeBox = <div>large orange box</div>
+// ** extra **
+function Box({className = '', style, size, ...otherProps}) {
+  const boxSizeClass = size ? `box--${size}` : ''
+  return (
+    <div
+      className={`box ${className} ${boxSizeClass}`}
+      style={{fontStyle: 'italic', ...style}}
+      {...otherProps}
+    />
+  )
+}
+
+// TIP: here it is className instead of class because it represents the dom properties....in browser the html has class and style (string) (HTML) compiled from className and style (JSX)..this className and style represents the properties in dom in dom style is not a string but an object and class is not class but className
+// console $0.class -> undefined but $0.className -> ''
+// JSX focus on dom property names rather than html attributes that's why className instead of class
+const smallBox = (
+  <Box size="small" style={{backgroundColor: 'lightblue'}}>
+    small lightblue box
+  </Box>
+)
+const mediumBox = (
+  <Box size="medium" style={{backgroundColor: 'lightpink'}}>
+    medium pink box
+  </Box>
+)
+const largeBox = (
+  <Box size="large" style={{backgroundColor: 'orange'}}>
+    large orange box
+  </Box>
+)
 
 function App() {
   return (
@@ -24,6 +51,9 @@ function App() {
       {smallBox}
       {mediumBox}
       {largeBox}
+      <Box size="small" style={{backgroundColor: 'lightblue'}}>
+        small lightblue box
+      </Box>
     </div>
   )
 }
