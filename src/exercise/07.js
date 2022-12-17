e31ca53ea53eabcd6db76dc95294ec07187b3c81
Joel Mathew Koshy <joelmathewkoshy@gmail.com>
2022-12-17 14:35:29 +0530

    Rendering lists

--- src/exercise/07.js
+++ src/exercise/07.js
@@ -22,6 +22,14 @@ function App() {
     setItems(items.filter(i => i.id !== item.id))
   }
 
+  // this is what i understood when we click on remove grape what happens is grape item is renamed to pear but <input has its own state it will still remain grape
+  // this is why we give a key prop
+
+  // from the video -> how will react know you removed an element in the array or if you removed a value and changed value of other elements in the array
+  // when removing elements in the example without the key prop, React is guessing which element was really removed sometimes it guesses right and sometimes wrong
+  // with the key prop react can track the element over time
+  // passes index of the array element to key prop is a bad idea since this is default behaviour when key prop is not given
+
   return (
     <div className="keys">
       <button disabled={items.length >= allItems.length} onClick={addItem}>
@@ -30,7 +38,7 @@ function App() {
       <ul style={{listStyle: 'none', paddingLeft: 0}}>
         {items.map(item => (
           // 🐨 add a key prop to the <li> below. Set it to item.id
-          <li>
+          <li key={item.id}>
             <button onClick={() => removeItem(item)}>remove</button>{' '}
             <label htmlFor={`${item.id}-input`}>{item.value}</label>{' '}
             <input id={`${item.id}-input`} defaultValue={item.value} />
